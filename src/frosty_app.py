a1cc2059ce291d3d5b2dadeed89023e56e81b7e5
Joshua Carroll <joshua.carroll@snowflake.com>
2023-06-16 15:49:24 -0700

    Refactor all the functions to better separate logic and make the app code much cleaner

--- src/frosty_app.py
+++ src/frosty_app.py
@@ -1,55 +1,27 @@
 import streamlit as st
-import re
-import prompts
-from utils import llm_chat, get_table_context
+from utils import get_table_context, get_response, render_message
 
 st.title("Frosty")
 
 _QUALIFIED_TABLE_NAME = "FROSTY_SAMPLE.CYBERSYN_SEC.SEC_REPORT_TEXT_LINKED"
+table_context = get_table_context(_QUALIFIED_TABLE_NAME)
 
 # Initialize the chat messages history
 if "messages" not in st.session_state.keys():
     st.session_state.messages = [{"role": "assistant", "content": "How can I help?"}]
-conn = st.experimental_connection("snowpark")
 
-def get_response(prompt):    
-    # prompt engineering LLM to generate sql code
-    st.session_state.messages.append(
-        {
-            "role": "system",
-            "hide": True,
-            "content": prompts.GEN_SQL.format(
-                user_prompt=prompt,
-                context=get_table_context(_QUALIFIED_TABLE_NAME),
-            ),
-        }
-    )
-    # Call LLM
-    response = llm_chat(st.session_state.messages)
-    message = {"role": "assistant", "content": response}
-    sql_match = re.search(r"<sql>(.*)</sql>", response, re.DOTALL)
-    if sql_match:
-        sql = sql_match.group(1)
-        message["sql"] = sql
-        # Execute the sql code
-        message["results"] = conn.query(sql)
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    st.session_state.messages.append(message)
+# display the existing chat messages   
+for message in st.session_state.messages:
+    render_message(message)
 
+# Get and render user input
 prompt = st.chat_input()
 if prompt:
-    get_response(prompt)
-    
-# display the chat messages   
-for message in st.session_state.messages:
-    if "hide" in message:
-        continue
-    if message["role"] == "user":
-        st.chat_message("user", background=True).write(message["content"])
-    else:
-        assistant = st.chat_message("assistant")
-        if "sql" in message:
-            assistant.code(message["sql"], language="sql")
-        if "results" in message:
-            assistant.dataframe(message["results"])
-        assistant.write(message["content"])
+    user_message = {"role": "user", "content": prompt}
+    st.session_state.messages.append(user_message)
+    render_message(user_message)
+
+    # Get and render LLM response
+    response = get_response(prompt, table_context)
+    render_message(response)
+    st.session_state.messages.append(response)
