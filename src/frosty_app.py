d9910256e8618d11047f6bb38e36e6012504cf2c
Joshua Carroll <joshua.carroll@snowflake.com>
2023-06-17 03:34:06 -0700

    Add hack to fix the shadow spinner issue

--- src/frosty_app.py
+++ src/frosty_app.py
@@ -22,6 +22,8 @@ def render_message(message):
         return
     role = message["role"]
     with st.chat_message(role, background=(role == "user")):
+        for _ in range(3):
+            st.empty() # This is a hack to fix the spinner shadow issue
         st.write(message["content"])
         if "results" in message:
             st.dataframe(message["results"])
@@ -35,17 +37,17 @@ if prompt:
     render_message(user_message)
     st.session_state.messages.append(user_message)
     # Call LLM
-    r = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        #engine="gpt-4",
-        messages=[{"role": m["role"], "content": m["content"]} for m in st.session_state.messages],
-    )
-    response = r.choices[0].message.content
-
-    # Parse the response for a SQL query
     with st.chat_message("assistant"):
+        r = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            #engine="gpt-4",
+            messages=[{"role": m["role"], "content": m["content"]} for m in st.session_state.messages],
+        )
+        response = r.choices[0].message.content
         st.write(response)
         message = {"role": "assistant", "content": response}
+
+        # Parse the response for a SQL query
         sql_match = re.search(r"```sql\n(.*)\n```", message["content"], re.DOTALL)
         if sql_match:
             # Execute and save the SQL query if available
