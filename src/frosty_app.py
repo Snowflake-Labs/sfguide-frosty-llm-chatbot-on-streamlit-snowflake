25419f71b552aaa77e8ac48dbfa3a1839b605c29
Joshua Carroll <joshua.carroll@snowflake.com>
2023-06-20 07:32:45 -0700

    Feedback from Adrien

--- src/frosty_app.py
+++ src/frosty_app.py
@@ -6,7 +6,7 @@ from prompts import get_system_prompt
 st.title("☃️ Frosty")
 
 # Initialize the chat messages history
-if "messages" not in st.session_state.keys():
+if "messages" not in st.session_state:
     st.session_state.messages = [
         {"role": "system", "content": get_system_prompt()},
         {"role": "assistant", "content": "How can I help?"}
@@ -26,44 +26,27 @@ for message in st.session_state.messages:
         if "results" in message:
             st.dataframe(message["results"])
 
-# Prompt for user input
-prompt = st.chat_input()
-
-if prompt:
+# Prompt for user input and process / save
+if prompt := st.chat_input():
     st.chat_message("user").write(prompt)
     st.session_state.messages.append({"role": "user", "content": prompt})
     # Call LLM
     with st.chat_message("assistant"):
-        # ----- non-streaming response -----
-        # with st.spinner("Thinking..."):
-        #     r = openai.ChatCompletion.create(
-        #         model="gpt-3.5-turbo",
-        #         #engine="gpt-4",
-        #         messages=[{"role": m["role"], "content": m["content"]} for m in st.session_state.messages],
-        #     )
-        #     response = r.choices[0].message.content
-        #     st.write(response)
-        # ----- non-streaming response code ends here -----
-
-        # ----- Streaming response -----
         response = ""
         resp_container = st.empty()
         for delta in openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
-            #engine="gpt-4",
             messages=[{"role": m["role"], "content": m["content"]} for m in st.session_state.messages],
             stream=True,
         ):
-                response += delta.choices[0].delta.get("content", "")
-                resp_container.markdown(response)
-        # ----- Streaming response code ends here -----
+            response += delta.choices[0].delta.get("content", "")
+            resp_container.markdown(response)
 
         message = {"role": "assistant", "content": response}
         # Parse the response for a SQL query and execute if available
         sql_match = re.search(r"```sql\n(.*)\n```", response, re.DOTALL)
         if sql_match:
             sql = sql_match.group(1)
-            message["sql"] = sql
             conn = st.experimental_connection("snowpark")
             message["results"] = conn.query(sql)
             st.dataframe(message["results"])
